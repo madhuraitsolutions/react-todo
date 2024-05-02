08fcaff3e68472feae8acf4bb73ab14a8a5ccf07
Kombiah <kombiahrk@yahoo.com>
2024-05-02 13:56:03 +0530

    Is used to display the task list and delete the task list

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,7 +1,27 @@
 import React from 'react'
 
-export const Board = () => {
+export const Board = ({ task, taskList, setTaskList }) => {
+
+  const handleDeleteTask = () => {
+    setTaskList(taskList.filter((todo) => todo !== task))
+
+    //  let removeTask = taskList.indexOf(task);
+    //  taskList.splice(removeTask, 1);
+    //  setTaskList((currentTasks => currentTasks.filter(todo => todo !== task)))
+
+    //  setTaskList([...taskList])
+  }
   return (
-    <div>Board</div>
+    <>
+      <div className='max-w-xl flex flex-col items-center justify-center border rounded-md p-6'>
+        <p>{task}</p>
+        <button 
+          className='bg-red-500 text-white px-4 rounded-lg py-1' 
+          onClick={handleDeleteTask}
+        >
+            delete
+        </button>
+      </div>
+    </>
   )
 }
