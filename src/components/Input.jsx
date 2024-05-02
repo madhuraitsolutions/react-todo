d5670f1922585791b62b89868ef01af89b03f8a2
Kombiah <kombiahrk@yahoo.com>
2024-05-02 12:35:43 +0530

    Input Compent , Added input and button elements

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,7 +1,22 @@
 import React from 'react'
 
 export const Input = () => {
+  const [input, setInput] = React.useState('')
+  const handleAddTask = (e) => {
+    e.preventDefault();
+  }
   return (
-    <div>Input Component</div>
+    <>
+      <form>
+        <input
+          className='rounded border-gray-300'
+          type="text"
+          placeholder='Add a new task'
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
+        <button onClick={handleAddTask}>Add</button>
+      </form>
+    </>
   )
 }
