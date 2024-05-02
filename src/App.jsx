5fc1f3d82f996e15b1ebe0e2d223dd32db13e477
Kombiah <kombiahrk@yahoo.com>
2024-05-02 12:38:35 +0530

    Create TaskList State Variable and passed into Input Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { Input } from "./components/Input"
 import "./App.css"
+import { useState } from "react"
 
 function App() {
+  const [taskList, setTaskList] = useState([])
 
   return (
     <div className="flex flex-col items-center justify-items-center ">
       <h1>02.TODO Board</h1>
-      <Input />
+      <Input taskList={taskList} setTaskList={setTaskList}/>
     </div>
   )
 }
