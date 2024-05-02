48412d9a97116c240d6f420fdee1afb591c918b5
Kombiah <kombiahrk@yahoo.com>
2024-05-02 13:56:52 +0530

    Used the react fragments to add aditional div block

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { Input } from "./components/Input"
+import { Board } from "./components/Board"
 import "./App.css"
 import { useEffect, useState } from "react"
 
@@ -9,16 +10,24 @@ function App() {
     console.log(taskList);
   }, [taskList])
   return (
-    <div className="flex flex-col items-center justify-center py-8 gap-4">
-      <h1 className="text-3xl font-semibold">02.TODO Board</h1>
-      <Input taskList={taskList} setTaskList={setTaskList}/>
-      <div>
+    <>
+      <div className="flex flex-col items-center justify-center py-8 gap-4">
+        <h1 className="text-3xl font-semibold">02.TODO Board</h1>
+        <Input taskList={taskList} setTaskList={setTaskList} />
+      </div>
+      <div className="grid grid-cols-3 gap-4">
         {/* list of tasks using map() */}
         {taskList.map((task, index) => (
-          <div key={index}>{task}</div>
-        ))} 
+          <Board
+            key={index}
+            task={task}
+            taskList={taskList}
+            setTaskList={setTaskList}
+          />
+        ))}
       </div>
-    </div>
+    </>
+
   )
 }
 
