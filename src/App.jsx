2785b69ce38181c05057ddb9a495d4eb9ee06985
Kombiah <kombiahrk@yahoo.com>
2024-05-02 12:36:26 +0530

    App.Css imported

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import { Input } from "./components/Input"
-
+import "./App.css"
 
 function App() {
 
   return (
-    <>
+    <div className="flex flex-col items-center justify-items-center ">
       <h1>02.TODO Board</h1>
       <Input />
-    </>
+    </div>
   )
 }
 
